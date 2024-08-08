71aa56e5c0b9fb4d9df7dc1aa2fa5eceaa4084eb
Mayank Singh <mr.mayank9900@gmail.com>
2024-08-08 18:26:59 +0530

    envpushed

--- Backend/index.js
+++ Backend/index.js
@@ -7,13 +7,22 @@ import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
 const app = express();
+dotenv.config();
+const FRONTEND_URL = process.env.FRONTEND_URL;
+console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
 
+//console.log(process.env.FRONTEND_URL);
 
 app.use(cors({
-    origin: 'https://booksstore-frontend.vercel.app',  // Replace with your frontend URL
-    methods: 'GET,POST,PUT,DELETE',  // Specify allowed methods
+   //origin: 'https://booksstore-frontend.vercel.app',  // Replace with your frontend URL
+    origin: FRONTEND_URL,
+
+   //origin:'http://localhost:5173',
+    //methods: 'GET,POST,PUT,DELETE',  // Specify allowed methods
     credentials: true  // If you need to allow credentials (cookies, HTTP authentication)
 }));
+
+//app.use(cors());
 
 app.use(express.json());
 
